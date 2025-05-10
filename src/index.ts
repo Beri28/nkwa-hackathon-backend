d3822615e629d1ed0db5bc77e645401cfa1c2689
Beri28 <cletusberinyuy@gmail.com>
2025-05-10 12:38:15 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -20,15 +20,16 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('src/frontend/public'))
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
-});
+app.use(express.static('./frontend/dist'))
+app.use(express.static('/frontend/assets'))
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Welcome to Express & TypeScript Server');
+// });
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/payment', paymentRouter);
 app.use('*',(req,res)=>{
-  res.sendFile('index.html',{root:'./src/frontend/dist'})
+  res.sendFile('index.html',{root:'./frontend/dist'})
 })
 
 app.listen(port, async () => {
